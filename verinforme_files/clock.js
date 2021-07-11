7c5a3b875282e311481984c17cc634d281ffc80d
synlabresults <87272182+synlabresults@users.noreply.github.com>
2021-07-11 16:59:33 +0200

    Add files via upload

---
+++ verinforme_files/clock.js
@@ -0,0 +1,23 @@
+﻿function currentTime() {
+    var date = new Date(); /* creating object of Date class */
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    hour = updateTime(hour);
+    min = updateTime(min);
+    sec = updateTime(sec);
+    if (document.getElementById("contenedor-reloj") != null) document.getElementById("contenedor-reloj").innerText = hour + " : " + min + " : " + sec; /* adding time to the div */
+    var t = setTimeout(function () { currentTime() }, 1000); /* setting timer */
+}
+
+function updateTime(k) {
+    if (k < 10) {
+        return "0" + k;
+    }
+    else {
+        return k;
+    }
+}
+
+
+currentTime();
---
+++ verinforme_files/informe.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+
+    if ($("#LabelInformacionPacienteDato1").html() == "" || $("#LabelInformacionPacienteDato1").html() == ":") $("#LabelInformacionPacienteDato1").parent().parent().hide();
+    if ($("#LabelInformacionPacienteDato2").html() == "" || $("#LabelInformacionPacienteDato2").html() == ":") $("#LabelInformacionPacienteDato2").parent().parent().hide();
+    if ($("#LabelInformacionPacienteDato3").html() == "" || $("#LabelInformacionPacienteDato3").html() == ":") $("#LabelInformacionPacienteDato3").parent().parent().hide();
+       
+    
+});
+
+function SeleccionarES() {
+    __doPostBack('lang_es', 'ES')
+}
+function SeleccionarEN() {
+    __doPostBack('lang_en', 'EN')
+}
+
